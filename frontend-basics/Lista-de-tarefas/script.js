0013755948e1b4b6b8caa3e51f6f1f4e301cde25
Larissa valleriano <larissavaleriano36@gmail.com>
2026-02-02 23:25:35 -0300

    .

--- frontend-basics/Lista-de-tarefas/script.js
+++ frontend-basics/Lista-de-tarefas/script.js
@@ -1,51 +1,13 @@
-const form = document.getElementById('form-tarefas');
-const input = document.getElementById('tarefa-input');
-const listaTarefas = document.getElementById('tarefas-lista');
-const progressBar = document.getElementById('file');
+let tarefas = []
 
-/*Função para atualizar a barra de tarefas*/
-function atualizarProgresso(){
-    const total = listaTarefas.querySelectorAll('li').length;
-    const completas = listaTarefas.querySelectorAll('li.completa').length;
-    console.log('total:', total, "Completas:", completas);
-
-    if (total === 0) {
-        progressBar.value = 0;
-    } else {
-        const porcentagem = (completas / total) * 100;
-        progressBar.value = porcentagem; 
-    }
+function adicionarTarefas() {
+    const inputTarefas = document.getElementById("tarefa-input")
+    let tarefa = inputTarefas.value.trim()
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // evita o carregamento da pagina
+function renderizarTarefas() {
+    const listaTarefas = document.getElementById("tarefas-lista")
+    listaTarefas.innerHTML = ""
+}
 
-    if (input.value.trim() === "") return;
 
-    // Cria um novo item de tarefa
-    const li = document.createElement('li');
-
-    li.innerHTML = `
-        <div class="tarefa-info">
-            <input type="checkbox" class="check-tarefa">
-            <span>${input.value}</span>
-        </div>
-        <button class="btn-excluir">excluir</button>
-    `;
-
-    const checkbox = li.querySelector('.check-tarefa');
-    checkbox.addEventListener('change', () => {
-        li.classList.toggle('completa');
-        atualizarProgresso();
-    })
-    
-    // remove a tarefa ao clicar no botão de excluir
-    li.querySelector('.btn-excluir').addEventListener('click', () => {
-        li.remove();
-        atualizarProgresso();
-    });
-
-    listaTarefas.appendChild(li);
-    input.value = ""; 
-    atualizarProgresso();
-});
